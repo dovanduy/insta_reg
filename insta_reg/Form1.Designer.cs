ff23099c81ba0aa6da0533b43f8afdd2343d9f78
stalerum <po@mospeu.ru>
2016-05-18 17:03:14 +0300

    2

--- insta_reg/Form1.Designer.cs
+++ insta_reg/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_StartRegMail = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nUD_CountMail = new System.Windows.Forms.NumericUpDown();
+            this.tB_Log = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_CountMail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_StartRegMail
+            // 
+            this.btn_StartRegMail.Location = new System.Drawing.Point(312, 11);
+            this.btn_StartRegMail.Name = "btn_StartRegMail";
+            this.btn_StartRegMail.Size = new System.Drawing.Size(75, 21);
+            this.btn_StartRegMail.TabIndex = 0;
+            this.btn_StartRegMail.Text = "Начать";
+            this.btn_StartRegMail.UseVisualStyleBackColor = true;
+            this.btn_StartRegMail.Click += new System.EventHandler(this.btn_StartRegMail_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Зарегать почту";
+            // 
+            // nUD_CountMail
+            // 
+            this.nUD_CountMail.Location = new System.Drawing.Point(120, 13);
+            this.nUD_CountMail.Name = "nUD_CountMail";
+            this.nUD_CountMail.Size = new System.Drawing.Size(118, 20);
+            this.nUD_CountMail.TabIndex = 2;
+            this.nUD_CountMail.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // tB_Log
+            // 
+            this.tB_Log.Location = new System.Drawing.Point(13, 42);
+            this.tB_Log.Multiline = true;
+            this.tB_Log.Name = "tB_Log";
+            this.tB_Log.Size = new System.Drawing.Size(577, 300);
+            this.tB_Log.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 354);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tB_Log);
+            this.Controls.Add(this.nUD_CountMail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_StartRegMail);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_CountMail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_StartRegMail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nUD_CountMail;
+        private System.Windows.Forms.TextBox tB_Log;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- insta_reg/Form1.cs
+++ insta_reg/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
 
 namespace insta_reg
 {
@@ -15,6 +18,83 @@ namespace insta_reg
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_StartRegMail_Click(object sender, EventArgs e)
+        {
+            /*
+            int CountMail = Convert.ToInt32(nUD_CountMail.Value);
+            ToLog(Convert.ToString(CountMail));
+            */
+
+            //1 этап
+            string url1 = "https://touch.mail.ru/cgi-bin/signup";
+            HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url1);
+            HttpWebResponse response1 = (HttpWebResponse)request1.GetResponse();
+            StreamReader reader1 = new StreamReader(response1.GetResponseStream());
+            //tB_Log.Text = reader1.ReadToEnd();
+            string cookies = String.IsNullOrEmpty(response1.Headers["Set-Cookie"]) ? "" : response1.Headers["Set-Cookie"];
+            tB_Log.Text = cookies;
+
+            //2 этап
+            //string url = "https://touch.mail.ru/api/v1/user/exists";
+            string url = "https://touch.mail.ru/cgi-bin/signup";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Host = "touch.mail.ru";
+            request.KeepAlive=false;
+            request.Accept = "*/*";
+            //request.Headers.Add(HttpRequestHeader.ori);
+            //origin
+            request.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            request.Referer = "https://touch.mail.ru/cgi-bin/signup";
+            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            request.Headers.Add(HttpRequestHeader.AcceptLanguage, "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+            request.Headers.Add(HttpRequestHeader.Cookie, cookies);
+            request.ServicePoint.Expect100Continue = false;
+            //post-данные
+            request.Method = "POST";
+            var postData = "login=fjdsdkfjd";
+            postData += "&email=fjdsdkfjd@mail.ru";
+            postData += "&domain=mail.ru";
+            postData += "&sex=male";
+            postData += "&birthday={\"year\":\"1994\",\"month\":\"10\",\"day\":\"20\"}";
+            postData += "&name={\"first\":\"Антон\",\"last\":\"Попов\"}";
+            HttpUtility.
+            HttpUtility.UrlEncode(postData);
+            var data = Encoding.UTF8.GetBytes(postData);
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            //UTF8Encoding encoding = new UTF8Encoding();
+
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            //tB_Log.Text = reader.ReadToEnd();
+            //MessageBox.Show(reader.ReadToEnd());
+            ToLog("ok");
+
+        }
+
+        // Вывод сообщения в лог
+        private void ToLog(string text)
+        {
+            tB_Log.AppendText(text+"\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://touch.mail.ru/cgi-bin/signup";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            tB_Log.Text = reader.ReadToEnd();
+
         }
     }
 }
